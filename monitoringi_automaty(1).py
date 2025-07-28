36b41b1b61226e3a753f80ee91313d37521e6804
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2025-07-28 10:59:11 +0200

    Update monitoringi_automaty(1).py

--- monitoringi_automaty(1).py
+++ monitoringi_automaty(1).py
@@ -194,7 +194,7 @@ if sekcja == 'Cykl Q3':
         result.to_excel(writer, index=False, sheet_name='Sheet1')
     excel_file1.seek(0)  # Resetowanie wskaźnika do początku pliku
 
-    nazwa_pliku1 = f"CYKL_Q2_{dzisiejsza_data}.xlsx"
+    nazwa_pliku1 = f"CYKL_Q3_{dzisiejsza_data}.xlsx"
     #Umożliwienie pobrania pliku Excel
     st.download_button(
         label='Pobierz',
@@ -212,7 +212,7 @@ if sekcja == 'Cykl Q3':
         result.to_excel(writer, index=False, sheet_name='Sheet1')
     excel_file1.seek(0)  # Resetowanie wskaźnika do początku pliku
 
-    nazwa_pliku = f"FM_CYKL_Q2_{dzisiejsza_data}.xlsx"
+    nazwa_pliku = f"FM_CYKL_Q3_{dzisiejsza_data}.xlsx"
     # Umożliwienie pobrania pliku Excel
     st.download_button(
         label='Pobierz nowy plik FORMUŁA MAX',
