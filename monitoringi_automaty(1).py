63f0eff14596f2a4e27ebcf01c68511c36038a30
ODPOUMiPmgNEUCA <mgroblica@neuca.pl>
2025-11-26 11:05:23 +0100

    Update monitoringi_automaty(1).py

--- monitoringi_automaty(1).py
+++ monitoringi_automaty(1).py
@@ -194,7 +194,7 @@ if sekcja == 'Collagen':
         result.to_excel(writer, index=False, sheet_name='Sheet1')
     excel_file1.seek(0)  # Resetowanie wskaźnika do początku pliku
 
-    nazwa_pliku1 = f"CYKL_Q3_{dzisiejsza_data}.xlsx"
+    nazwa_pliku1 = f"COLLAGEN_{dzisiejsza_data}.xlsx"
     #Umożliwienie pobrania pliku Excel
     st.download_button(
         label='Pobierz',
@@ -212,7 +212,7 @@ if sekcja == 'Collagen':
         result.to_excel(writer, index=False, sheet_name='Sheet1')
     excel_file1.seek(0)  # Resetowanie wskaźnika do początku pliku
 
-    nazwa_pliku = f"FM_CYKL_Q3_{dzisiejsza_data}.xlsx"
+    nazwa_pliku = f"FM_COLLAGEN_{dzisiejsza_data}.xlsx"
     # Umożliwienie pobrania pliku Excel
     st.download_button(
         label='Pobierz nowy plik FORMUŁA MAX',
